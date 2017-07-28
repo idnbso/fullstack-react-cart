54764d7140bd5f1eda0b2edbaf8bd16a0c542dbd
idnbso <idanbusso@gmail.com>
2017-07-28 16:19:11 +0300

    hello webpack

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    entry: './client/index.js',
+    output: {
+        path: __dirname + '/client',
+        filename: 'bundle.js',
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader',
+                query: { presets: [ 'react', 'es2015' ] },
+            },
+        ],
+    },
+};
